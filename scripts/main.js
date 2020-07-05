6723b043bc20de4b48c68b1abd0fbfed6a815558
Angel Buzany <abuzzany@gmail.com>
2020-07-04 21:00:14 -0500

    Moves lógicto to a new class Capculator

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,96 @@
+class Capculator{
+    constructor(display){
+        this.display = display
+
+        this.prevOperand = ''
+        this.currentOperand = ''
+        this.operator = ''
+        this.bufferOperands = []
+        this.stackOperations = []
+    }
+
+    addOperand(number){
+        this.prevOperand = this.prevOperand + number
+        this.currentOperand = number
+    
+        this.bufferOperands.push(number)
+
+        this.display.innerHTML = this.prevOperand
+    }
+
+    addOperator(operator){
+        if (this.prevOperand === '') return
+    
+        this.emptyBufferOperands()
+        this.operator = operator
+        this.stackOperations.push(operator)
+        this.prevOperand = ''
+    }
+
+    calculateResult(){
+        if (this.prevOperand === '' || this.operator === '') return
+
+        this.emptyBufferOperands()
+    
+        let result = ''
+    
+        switch(operator){
+            case "+":
+                result = parseFloat(stackOperations[0]) + parseFloat(stackOperations[2])
+                break
+            case "-":
+                result = parseFloat(stackOperations[0]) - parseFloat(stackOperations[2])
+                break
+            case "*":
+                result = parseFloat(stackOperations[0]) * parseFloat(stackOperations[2])
+                break
+            case "/":
+                result = parseFloat(stackOperations[0]) / parseFloat(stackOperations[2])
+                break
+        }
+    
+        this.stackOperations = []
+        this.operator = ''
+        this.prevOperand = ''
+    
+        this.display.innerHTML = result
+    }
+
+    emptyBufferOperands(){
+        if(bufferOperands.length != 0){
+            stackOperations.push(bufferOperands.join(''))
+            bufferOperands = []
+        }
+    }
+
+    clear(){
+        this.operator = ''
+        this.prevOperand = ''
+        this.stackOperations  = []
+        this.bufferOperands = []
+    }
+}
+
 const display = document.getElementById('')
 
-let prevOperand = ''
-let currentOperand = ''
-let operator = ''
-let bufferOperands = []
-let stackOperations = []
+console.log(display)
+
+const capculator = new Capculator(display)
 
 function onClickOperand(event){
     value = event.target.value
-    prevOperand = prevOperand + value
-    currentOperand = value
 
-    bufferOperands.push(value)
-    this.display.innerHTML = prevOperand
+    capculator.addOperand(value)
 }
 
 function onClickOperator(event){
-    if (prevOperand === '') return
-    
     value = event.target.value
-    emptyBufferOperands()
-    operator = value
-    stackOperations.push(operator)
-    prevOperand = ''
+
+    capculator.addOperator(value)
 }
 
 function onClickEqual(event){
-    if (prevOperand === '' || operator === '') return
-
-    emptyBufferOperands()
-
-    result = ''
-
-    switch(operator){
-        case "+":
-            result = parseFloat(stackOperations[0]) + parseFloat(stackOperations[2])
-            break
-        case "-":
-            result = parseFloat(stackOperations[0]) - parseFloat(stackOperations[2])
-            break
-        case "*":
-            result = parseFloat(stackOperations[0]) * parseFloat(stackOperations[2])
-            break
-        case "/":
-            result = parseFloat(stackOperations[0]) / parseFloat(stackOperations[2])
-            break
-    }
-
-    stackOperations = []
-    operator = ''
-    prevOperand = ''
-
-    this.display.innerHTML = result
+   capculator.calculateResult()
 }
 
 function onClickPoint(event){
@@ -60,20 +99,6 @@ function onClickPoint(event){
 }
 
 function onClickClear(event){
-    clear()
+    capculator.clear()
     this.display.innerHTML = 0
-}
-
-function emptyBufferOperands(){
-    if(bufferOperands.length != 0){
-        stackOperations.push(bufferOperands.join(''))
-        bufferOperands = []
-    }
-}
-
-function clear(){
-    this.operator = ''
-    this.prevOperand = ''
-    this.stackOperations  = []
-    this.bufferOperands = []
 }
