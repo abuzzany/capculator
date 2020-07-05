aac40288142eaf706e58a4d5034853b1afb13359
Angel Buzany <abuzzany@gmail.com>
2020-07-04 20:12:55 -0500

    Add logic to concat operands on the display

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 const display = document.getElementById('')
 
+let prevOperand = ''
+let currentOperand = ''
+let operator = ''
+
 function onClickOperand(event){
     value = event.target.value
-    this.display.innerHTML = value
+    prevOperand = prevOperand + value
+    currentOperand = value
+
+    this.display.innerHTML = prevOperand
 }
 
 function onClickOperator(event){
