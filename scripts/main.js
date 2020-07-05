d321211a7d6c09272ba6a9364fc53fb97e1d3962
Angel Buzany <abuzzany@gmail.com>
2020-07-04 20:02:22 -0500

    Delete id from event functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,26 @@
 const display = document.getElementById('')
 
 function onClickOperand(event){
-    id = event.target.id
     value = event.target.value
     this.display.innerHTML = value
 }
 
 function onClickOperator(event){
-    id = event.target.id
     value = event.target.value
     this.display.innerHTML = value
 }
 
 function onClickEqual(event){
-    id = event.target.id
     value = event.target.value
     this.display.innerHTML = value
 }
 
 function onClickPoint(event){
-    id = event.target.id
     value = event.target.value
     this.display.innerHTML = value
 }
 
 function onClickClear(event){
-    id = event.target.id
     value = event.target.value
     this.display.innerHTML = value
 }
