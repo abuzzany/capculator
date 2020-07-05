0fc34d517f02864fcc810fb1b1c510fa78ce5fe3
Angel Buzany <abuzzany@gmail.com>
2020-07-04 20:15:03 -0500

    Add buffer to storage operands

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,21 @@ const display = document.getElementById('')
 let prevOperand = ''
 let currentOperand = ''
 let operator = ''
+let bufferOperands = []
 
 function onClickOperand(event){
     value = event.target.value
     prevOperand = prevOperand + value
     currentOperand = value
 
+    bufferOperands.push(prevOperand)
     this.display.innerHTML = prevOperand
 }
 
 function onClickOperator(event){
     value = event.target.value
-    this.display.innerHTML = value
+    
+
 }
 
 function onClickEqual(event){
