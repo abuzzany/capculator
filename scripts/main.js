44008d00083fbc081c6158c407f4661aab6cfd31
Angel Buzany <abuzzany@gmail.com>
2020-07-04 21:39:19 -0500

    Add suport for float point operations

--- scripts/main.js
+++ scripts/main.js
@@ -9,11 +9,9 @@ class Capculator{
     }
 
     addOperand(number){
+        if(number === '.' && this.bufferOperands.includes(number)) return
         this.currentOperand = this.currentOperand + number
-    
         this.bufferOperands.push(number)
-
-        this.display.innerHTML = this.currentOperand
     }
 
     addOperator(operator){
@@ -29,12 +27,11 @@ class Capculator{
         if (this.currentOperand === '' || this.operator === '') return
 
         this.emptyBufferOperands()
+
+        this.currentOperand = this.calculate()
     
         this.stackOperations = []
         this.operator = ''
-        this.currentOperand = ''
-    
-        this.display.innerHTML = this.calculate
     }
 
     calculate(){
@@ -42,6 +39,7 @@ class Capculator{
     
         switch(this.operator){
             case "+":
+                console.log(result)
                 result = parseFloat(this.stackOperations[0]) + parseFloat(this.stackOperations[2])
                 break
             case "-":
@@ -71,6 +69,10 @@ class Capculator{
         this.stackOperations  = []
         this.bufferOperands = []
     }
+
+    updateDisplay(){
+        this.display.innerHTML = this.currentOperand
+    }
 }
 
 const display = document.getElementById('display')
@@ -81,6 +83,7 @@ function onClickOperand(event){
     value = event.target.value
 
     capculator.addOperand(value)
+    capculator.updateDisplay()
 }
 
 function onClickOperator(event){
@@ -91,11 +94,14 @@ function onClickOperator(event){
 
 function onClickEqual(event){
    capculator.calculateResult()
+   capculator.updateDisplay()
 }
 
 function onClickPoint(event){
     value = event.target.value
-    this.display.innerHTML = value
+
+    capculator.addOperand(value)
+    capculator.updateDisplay()
 }
 
 function onClickClear(event){
