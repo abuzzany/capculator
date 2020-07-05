a3dca09d27563e3716f87eb9741fba9537f77add
Angel Buzany <abuzzany@gmail.com>
2020-07-04 20:01:24 -0500

    Separate events for operands, operators, etc

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,30 @@
 const display = document.getElementById('')
 
-function onClicKey(event){
+function onClickOperand(event){
+    id = event.target.id
+    value = event.target.value
+    this.display.innerHTML = value
+}
+
+function onClickOperator(event){
+    id = event.target.id
+    value = event.target.value
+    this.display.innerHTML = value
+}
+
+function onClickEqual(event){
+    id = event.target.id
+    value = event.target.value
+    this.display.innerHTML = value
+}
+
+function onClickPoint(event){
+    id = event.target.id
+    value = event.target.value
+    this.display.innerHTML = value
+}
+
+function onClickClear(event){
     id = event.target.id
     value = event.target.value
     this.display.innerHTML = value
