ffc8ab2a0c24cecb9f23fcefd97fbde40a444f45
Angel Buzany <abuzzany@gmail.com>
2020-07-13 04:08:12 -0500

    Generate documentation

--- src/scripts/capculator.js
+++ src/scripts/capculator.js
@@ -1,5 +1,11 @@
 import CapculatorEngine from './capculatorEngine'
+
+/** This class works as a interface to handle UI calculator. */
 class Capculator {
+  /**
+   * Create an instace of Capculator.
+   * @param {HTMLDivElement} display - The display value.
+   */
   constructor (display) {
     this.display = display
     this.prevOperand = ''
@@ -11,6 +17,11 @@ class Capculator {
     this.lastStackOperations = []
   }
 
+  /**
+   * Add an operand to the operation stack.
+   * @param {string} operation - The operation value.
+   * @return {void}
+   */
   addOperand (number) {
     if (this._operandIsFloatPoint(number) && this._currentOperandContainsFloatPoint()) return
     if (this._operandIsZero(number) && this._currentOperandStartsWithZero() && !this._currentOperandContainsFloatPoint()) return
@@ -23,6 +34,12 @@ class Capculator {
     this.currentOperand = this.currentOperand + number
   }
 
+  /**
+   * Add an operand to the operation stack and compute the operation
+   * if the operation stack is completed.
+   * @param {string} operator - The operator value.
+   * @return {void}
+   */
   addOperator (operator) {
     if (this._currentOperandIsEmpty()) return
 
@@ -50,6 +67,10 @@ class Capculator {
     this.currentOperator = operator
   }
 
+  /**
+   * Update an HTMLDivElement that works as an UI display.
+   * @return {void}
+   */
   updateDisplay () {
     if (this._currentOperandIsEmpty()) {
       this.display.innerText = '0'
@@ -58,6 +79,11 @@ class Capculator {
     this.display.innerText = this.currentOperand.toString()
   }
 
+  /**
+   * Clear an HTMLDivElement  that works as an UI display and also
+   * clear the stack operations.
+   * @return {void}
+   */
   clearAll () {
     this.prevOperand = ''
     this.currentOperand = ''
@@ -67,11 +93,20 @@ class Capculator {
     this.lastStackOperations = []
   }
 
+  /**
+   * Clear an HTMLDivElement and that works as an UI display and also
+   * clear the las operand setted.
+   * @return {void}
+   */
   clear () {
     this.currentOperand = ''
     this.bufferOperands = []
   }
 
+  /**
+   * Empty the bufferOperands into the operation stack.
+   * @return {void}
+   */
   _emptyBufferOperands () {
     if (this.bufferOperands.length !== 0) {
       this.stackOperations.push(this.bufferOperands.join(''))
@@ -79,6 +114,10 @@ class Capculator {
     }
   }
 
+  /**
+   * Create an instance of CapculatorEngine and compute the stack operations.
+   * @return {void}
+   */
   _computeOperation () {
     if (!this._isReadyToComputeOperation()) return
 
@@ -90,6 +129,10 @@ class Capculator {
     this.stackOperations.push(this.currentOperand)
   }
 
+  /**
+   * Assing the las operation stack to compute it again.
+   * @return {void}
+   */
   _assignLastOperation () {
     this.stackOperations[1] = this.lastStackOperations[1]
     this.stackOperations[2] = this.lastStackOperations[2]
@@ -99,30 +142,62 @@ class Capculator {
     this.currentOperator = this.lastStackOperations[1]
   }
 
+  /**
+   * Guard to check if the operand is a float point.
+   * @param {string} number - The number value.
+   * @return {boolean}
+   */
   _operandIsFloatPoint (number) {
     return (number === '.')
   }
 
+  /**
+   * Guard to check if the current operand contains a float point.
+   * @return {boolean}
+   */
   _currentOperandContainsFloatPoint () {
     return this.currentOperand.includes('.')
   }
 
+  /**
+   * Guard to check if the operand is zero.
+   * @param {string} number - The number value.
+   * @return {boolean}
+   */
   _operandIsZero (number) {
     return (number === '0')
   }
 
+  /**
+   * Guard to check if the current operand starts with zero.
+   * @return {boolean}
+   */
   _currentOperandStartsWithZero () {
     return (this.currentOperand.startsWith('0'))
   }
 
+  /**
+   * Guard to check if the current operand is empty.
+   * @return {boolean}
+   */
   _currentOperandIsEmpty () {
     return (this.currentOperand === '')
   }
 
+  /**
+   * Guard to check if the stack operarion is completed to compute an operation.
+   * @return {boolean}
+   */
   _isReadyToComputeOperation () {
-    return (this.prevOperand !== '' && this.currentOperator !== '' && this.currentOperand !== '')
+    return (
+      this.prevOperand !== '' && this.currentOperator !== '' && this.currentOperand !== ''
+    )
   }
 
+  /**
+   * Guard to check if the there was computed an operation.
+   * @return {boolean}
+   */
   _wasComputedAnOperation () {
     return (this.lastStackOperations.length > 0)
   }
--- src/scripts/capculatorEngine.js
+++ src/scripts/capculatorEngine.js
@@ -1,10 +1,19 @@
+/** This class works to compute basic arithmetic operation. */
 class CapculatorEngine {
+   /**
+   * Create an instace of CapculatorEngine.
+   * @param {Array} stackOperations - The stackOperations value.
+   */
   constructor (stackOperations) {
     this.firstOperand = stackOperations[0]
     this.operator = stackOperations[1]
     this.secondOperand = stackOperations[2]
   }
 
+  /**
+   * Compute an arithmetic operation.
+   * @retutn {Float} result - The result value.
+   */
   compute () {
     let result = 0
 
